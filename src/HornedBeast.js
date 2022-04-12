539ab849b71d53ef27650ebec2a7eaa99ccee58f
Darius <darius.owens.j@gmail.com>
2022-04-12 16:13:01 -0700

    finished feature 1

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+
 class HornedBeast extends React.Component {
   render() {
     return (
       <div>
-        <h2>{this.props.name }</h2>
-        <image src='https://www.fillmurray.com/640/360' alt='Placeholder image'>Placeholder image</image>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <h2>{this.props.title }</h2>
+        <img 
+        src={this.props.image_url} 
+        alt={this.props.image_url} 
+        />
+        <p>{this.props.description}</p>
       </div>
 
     )
--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,40 @@
 import React from 'react';
+import data from './HornedB.json';
 import HornedBeast from './HornedBeast';
 
 class Main extends React.Component {
   render() {
+    let hornB = [];
+    data.forEach((animal) => {
+      hornB.push(
+        <article>
+          <HornedBeast 
+                title={animal.title}
+                image_url={animal.image_url}
+                description={animal.description} 
+              />
+          {/* <HornedBeast 
+                title={animal.title}
+                image_url={animal.image_url}
+                descripton={animal.description} 
+        />
+         <HornedBeast
+              title={animal.title}
+              image_url={animal.image_url}
+              descripton={animal.description} 
+        /> */}
+          </article>
+      )
+    });
+
     return (
       <main>
-        <HornedBeast name='UniWhal' imgUrl='Picture' text='Description'/>
-        <HornedBeast name='Rhino Family' imgurl='Picture' text='Description'/>
-     </main>
-      
-     
+        {hornB}
+      </main>
+
+
     )
 
   }
-}
+};
 export default Main;
